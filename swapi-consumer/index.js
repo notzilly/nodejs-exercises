cbfdc56df2d08c5e4b33de4ddfecdcc85dc0d1b1
notzilly <imnotzilly@gmail.com>
2022-07-28 21:03:17 -0300

    using promises to retrieve all uids from starships

--- swapi-consumer/index.js
+++ swapi-consumer/index.js
@@ -1,9 +1,27 @@
 import fetch from 'node-fetch'
 
-const starships = await fetch("https://www.swapi.tech/api/starships")
-    .then(res => res.json())
-    .catch(err => console.error(err))
+const apiUrl = 'https://www.swapi.tech/api/starships/'
 
-starships.results.map((ss) => {
-    console.log(ss.name);
-})
+async function retrievePages() {
+    const firstRes = await fetch(apiUrl)
+    const body = await firstRes.json();
+
+    let pagePromises = []
+    for (let page = 1; page < body.total_pages + 1; page++) {
+        pagePromises.push(fetch(`${apiUrl}?page=${page}&limit=10`))
+    }
+
+    return Promise.all(pagePromises)
+}
+
+
+const pagePromises = await retrievePages()
+const pages = await Promise.all(pagePromises.map(res => res.json()))
+
+const starshipsUids = pages
+    .flatMap(page => page.results)
+    .map(starship => starship.uid)
+
+
+console.log(starshipsUids)
+
